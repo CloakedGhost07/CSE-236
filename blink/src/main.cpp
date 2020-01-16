4f1840c7766d20ec75050fb43f0b21662ff0e079
CloakedGhost07 <46792241+CloakedGhost07@users.noreply.github.com>
2020-01-16 11:35:45 -0600

    Update main.cpp
    Make the lights only do cycle once

--- blink/src/main.cpp
+++ blink/src/main.cpp
@@ -7,9 +7,34 @@ This is a simple blink program for the arduino board
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+  // long on
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  // short off
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+  // short on
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  // short off
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+  // short on
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  // long off
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  // long on
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  // long off
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
 
 void loop() {
+  /*
   // long on
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
@@ -34,4 +59,5 @@ void loop() {
   // long off
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+  */
 }
